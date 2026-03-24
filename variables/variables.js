8832584ab47a4dbab3dd7685916afc23598de81c
jatink2000 <jatink8059@gmail.com>
2026-03-24 10:18:15 +0530

    sh

---
+++ variables/variables.js
@@ -0,0 +1,90 @@
+
+
+
+// variables : variables is like a container , which is used to store the data or value like number , string , char , etc . 
+
+    // how to create a variable in java script :  to create a variable in java script first declare the variable name and then assign the value / data . 
+
+        // snytex : 
+            // variablename=data /value 
+
+            // students=20   // number 
+            // myname="groot academy "   // string 
+            // year=2026
+
+    // how to access the variables : to access the variables use the document.write or console.log function , inside that function write the variable name , 
+
+        // document.writeln(year)
+        // console.log(myname)
+
+
+
+    // general rules of a variables : 
+        // 1. variables name can start with letter or underscore (_) . 
+                    // _year=36
+        // 2. variables name cannot start with number . 
+                    // 2year=49
+        // 3. variables name can contain letter number and $ symbol .
+                    // s4tu$dents=20 
+        // 4. variables name not create predefine keyword like function , class etc . 
+        // 5. variable name are case senstive .
+                    // year=20
+                    // console.log(Year) 
+        // 6. variables name cannot contain any extra / white space . 
+                    // ye ar=30
+
+
+
+    // how to change / modify  the variable value : 
+        // students=40
+        // document.writeln(students) // 40 
+        // students=50 // chnage /modify 
+        // document.writeln(students) // 50 
+
+
+
+    // methods to declare the variables : to declare the variables 4 ways : 
+        // 1. old java script : 
+            // 1. automatic / direct declare 
+            // 2. var   : 
+                    // var a=20;
+                    // console.log(a)
+                    // a=30  // modify 
+                    // console.log(a)
+
+                    // var a=70 // redeclare 
+                    // console.log(a) 
+
+
+        // 2. modren java script : 
+            // 1. let  : 
+                    // let a=40;
+                    // console.log(a)
+
+                    // a=60;
+                    // console.log(a)
+
+
+                    // let a=90
+                    // console.log(a)
+
+
+            // 2. const  : 
+
+                    // const a=50;
+                    // console.log(a)
+
+
+                    // a=80
+                    // console.log(a)
+
+
+
+    // let a=40
+    // let b=10
+    // let c ; 
+    // c=a  // 40 
+    // a=b // 10 
+    // b=c  // 40 
+    // console.log(a) 
+    // console.log(b)
