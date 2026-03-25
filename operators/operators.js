543e6a9c0dc1eb134904679818b2fc9ff84f2008
jatink2000 <jatink8059@gmail.com>
2026-03-25 10:19:29 +0530

    sh

---
+++ operators/operators.js
@@ -0,0 +1,78 @@
+
+
+// userinput : the userinput is used to get / access the data /value from user . and the prompt function is used to userinput in java script . 
+
+
+    // let a ; 
+    // a=prompt("enter a number")
+    // document.writeln("your number is ",a)
+
+
+
+// operators : operators is like a symbol . which is used to perform operation between variables and operands . 
+        // operands is number or value . 
+
+    // types of operators : there are different types of operators . 
+
+
+    // 1. arithmatics operators  :the arithmatics operators is used to perform mathematics task. 
+            // symbol : +,-,*,/,%(modules),**(power),++,--
+
+            // let a=parseInt(prompt("enter a number"))  // int 
+            // let b=parseInt(prompt("enter a number")) // int
+            // document.writeln(a**b)
+
+
+            // pre  :
+            //  let a=3 
+            //  document.writeln(++a,"<br/>") // ++3 : 4
+            //  document.writeln(--a) // --4 : 3
+
+
+            // post : 
+            // let a=5;
+            // let b=++a + a-- + 6 
+            // document.writeln(b) // ++5: 6+6--+6
+            // document.writeln(a--) // 5-- : 4
+            // document.writeln(a) // 4 :  4
+
+
+    // 2. assignment operators : the assignment operators is used to assign the variables and operands . 
+            // symbol : =,+=,-=,*=,/=,%= 
+
+            // let a=45 ; 
+            // a%=6;
+            // document.writeln(a) // 45+=6 : 51
+
+
+
+    // 3. comparision operators :  the comparision operators is used to compair the variables and operands .and return the boolean data (true & false).   
+            // symbol : ==,<,>,<=.>=,!=
+
+                // let a=30;
+                // let b=30;
+                // document.writeln(a!=b)
+
+
+
+    // 4. logical operators : the logical operators is used to logics . 
+
+        // 1. && (and) : it return true if all conditions is true otherwise it return false . 
+            // document.writeln(true && true && true && true) 
+
+            // let a=30;
+            // let b=18;
+            // document.writeln(a>b && a>=b)
+
+
+        // 2. || (or) : it return true if anyone conditions is true otherwise it return false . 
+
+                // document.writeln(false || false || false || false) 
+
+
+        // 3. ! (not): the not operators reverse the condition . 
+                // !true : false 
+                // !false : true
+
+
+                // document.writeln(!false)
