b1e83ecc3938f32730a074357b4f6d540d2fce4f
jatink2000 <jatink8059@gmail.com>
2026-03-26 10:15:18 +0530

    sj

---
+++ control statment/controlstatment.js
@@ -0,0 +1,112 @@
+
+
+// controlstatment : the control statment is used to perfrom different action based on different statment . 
+
+// 1. if : if is specify a block of code to be excute if the condition is true. 
+
+    // syntex : 
+
+    // if(condition){
+    //     code to be excute . 
+    // }
+
+
+    // let a=prompt("enter a number: ")
+    // if(a<50){
+    //     document.writeln("your number is less then 50 & your number is ", a)
+    // }
+
+
+// 2. else : else is specify a block of code to be excute if the same condition is false .  
+
+    // syntex : 
+    // if(condition){
+    //     code to be excute / statment 1
+    // }
+    // else{
+    //     code to be excute / statment 2 
+    // }
+
+
+
+    // let a=prompt("enter a number: ")
+    // if(a<50){
+    //     document.writeln("your number is less then 50 & your number is ", a)
+    // }
+    // else{
+    //     document.writeln("your number is greater then 50 & your number is ", a)
+    // }
+
+
+// 3. else if :  else if is specify a block of code to be excute if we check a new condition after the first condition is false . 
+
+     // syntex : 
+    // if(condition){
+    //     code to be excute / statment 1
+    // }
+    // else if(condition){
+    //     code to be excute / statment 2
+    // }
+    // else if(condition){
+    //     code to be excute / statment 3
+    // }
+    // else{
+    //     code to be excute / statment 4 
+    // }
+
+
+
+    // if(false){
+    //     document.writeln("hy")
+    // }
+    // else if(false){
+    //     document.writeln("hlo")
+    // }
+    // else if(false){
+    //     document.writeln("hlo .... ")
+    // }
+    // else{
+    //     document.writeln("groot")
+    // }
+
+
+
+    // let a=prompt("enter a number: ")
+    // if(a<50){
+    //     document.writeln("your number is less then 50 & your number is ", a)
+    // }
+    // else if(a<100){
+    //     document.writeln("your number is less then 100 but greater then 49")
+    // }
+    // else{
+    //     document.writeln("your number is greater then 100 & your number is ", a)
+    // }
+
+
+
+
+    // Q 8:
+    // let a=prompt("enter first number") 
+    // let b=prompt("enter second number") 
+    // if(a>b){
+    //     document.writeln("first number is maximum")
+    // }
+    // else if(a==b){
+    //     document.writeln("both numbers are equal")
+    // }
+    // else{
+    //     document.writeln("second number is maximum")
+    // }
+
+
+    // Q 10 : 
+    let a=prompt("enter a number")
+    if(a>0){
+        document.writeln("your number is positive")
+    }
+    else if(a<0){
+        document.writeln("your number is negative")
+    }
+    else{
+        document.writeln("your number is zero")
+    }
