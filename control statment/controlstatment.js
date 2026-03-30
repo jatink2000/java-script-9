ebb0d6912a9857184b59ad2eb9958d32c17f5780
jatink2000 <jatink8059@gmail.com>
2026-03-30 10:30:07 +0530

    sh

--- control statment/controlstatment.js
+++ control statment/controlstatment.js
@@ -100,13 +100,125 @@
 
 
     // Q 10 : 
-    let a=prompt("enter a number")
-    if(a>0){
-        document.writeln("your number is positive")
-    }
-    else if(a<0){
-        document.writeln("your number is negative")
-    }
-    else{
-        document.writeln("your number is zero")
-    }
+    // let a=prompt("enter a number")
+    // if(a>0){
+    //     document.writeln("your number is positive")
+    // }
+    // else if(a<0){
+    //     document.writeln("your number is negative")
+    // }
+    // else{
+    //     document.writeln("your number is zero")
+    // }
+
+
+    // Q 17 : 
+
+    // let a = prompt("enter week number: ")
+    // if(a==1){
+    //     document.writeln("monday")
+    // }
+    // else if(a==2){
+    //     document.writeln("tuesday")
+    // }
+    // else{
+    //     document.writeln("not a week number")
+    // }
+
+
+    // Q 20  :
+    // let cost=prompt("enter cost price of a product")
+    // let selling=prompt("enter selling price of a product")
+    // if(cost>selling){
+    //     document.writeln("loss :",cost-selling)
+    // }
+    // else{
+    //     document.writeln("profit :",selling-cost)
+    // }
+
+
+
+
+    // Q 21 : 
+    // let cm=prompt("enter length in centimeter")
+    // let m=cm/100 
+    // let km=cm/100000 
+    // document.writeln("meter is:",m,"<br/>")
+    // document.writeln("km is:",km)
+
+
+
+
+    // ternary operator :
+    // syntex : 
+        // (condition) ? statment1 : statment2 
+
+
+    // let num1=prompt("enter first number");
+    // let num2=prompt("enter second number");
+    // (num1>num2) ? console.log("first number is maximum") : console.log("second number is maximum")
+
+
+
+    // switch case : 
+    // syntex : 
+        // switch(expression){
+        //     case condition: 
+        //         code to be excute 
+        //         break ; 
+             
+        //     case condition: 
+        //         code to be excute 
+        //         break ; 
+
+        //     case condition: 
+        //         code to be excute 
+        //         break ; 
+            
+        //     case condition: 
+        //         code to be excute 
+        //         break ; 
+
+        //     default:
+        //         code to be excute  
+        // }
+
+
+        // let a=parseInt(prompt("enter week number:"))
+        // switch(a){
+        //     case 1:
+        //         document.writeln("monday")
+        //         break;
+
+        //     case 2:
+        //         document.writeln("tuesday")
+        //         break;
+
+        //     case 3:
+        //         document.writeln("wednesday")
+        //         break;
+
+        //     case 4:
+        //         document.writeln("thrusday")
+        //         break;
+
+        //     case 5:
+        //         document.writeln("friday")
+        //         break;
+
+
+        //     case 6:
+        //         document.writeln("saturday")
+        //         break;
+
+
+        //     case 7:
+        //         document.writeln("sunday")
+        //         break;
+
+        //     default:
+        //         document.writeln("not a week number")                
+        // }
+
+
+        // break : 
---
+++ loops/loops.js
@@ -0,0 +1,54 @@
+
+
+
+// loops  :
+
+// types of loops : there are three types of loops : 
+
+    // start , condition , step  
+
+
+    // 1. for loop : 
+
+    // for(start;condition;step){
+    //     code to be excute . 
+    // }
+    
+    
+    // for(i=10;i<=110;i++){
+    //     document.writeln(i)
+    // }
+
+
+
+    // 2. while loop : 
+
+    // start 
+    // while(condition){
+    //     code to be excute 
+    //     step 
+    // }
+
+
+    // i=1 
+    // while(i<=100){
+    //     document.writeln(i )
+    //     i++
+    // }
+
+
+
+    // 3. do while loop : 
+
+    // start 
+    // do{
+    //     code to be excute 
+    //     step 
+    // }while(condition)
+
+
+    // i=100
+    // do{
+    //     document.writeln("groot ")
+    //     i--
+    // }while(i>=1)
