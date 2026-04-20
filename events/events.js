ffbc92ea499a52f8fe0c25bc3ffd2ae62ed2f27e
jatink2000 <jatink8059@gmail.com>
2026-04-20 10:10:34 +0530

    j

---
+++ events/events.js
@@ -0,0 +1,60 @@
+
+
+// events : events are the actions that excute the java script code when the users can happen something in browser . 
+
+// there are two types of events : 
+// 1. HTML events : the html events is used as a attributes . 
+// some examples of html events is : 
+//         1. when an users clicks the mouse . 
+//         2. when an input is changed . 
+//         3. when a user move the mouse over of an element . 
+//         4. when a user move the mouse out of an element .
+
+
+// 1. onclick events :the onclick event when an users clicks the mouse of an element . 
+
+function groot1() {
+    document.getElementById("tech").innerHTML = "Hello Js"
+    document.getElementById("tech").style.color = "red"
+}
+
+function groot2() {
+    document.getElementById("tech").innerHTML = "Hello Html"
+    document.getElementById("tech").style.color = "black"
+}
+
+
+// 2. onchange event : 
+
+
+
+function groot3(){
+let inputvalue = document.getElementById("input").value
+    document.getElementById("displayname").innerHTML=inputvalue
+}
+
+
+
+// 3. onmouseover event : the onmouseover event is used when a user move the mouse over of an element . 
+
+    function groot4(){
+        document.getElementById("tech2").innerHTML="Hello Js"
+        document.getElementById("tech2").style.backgroundColor="blue"
+    }
+
+
+
+    // 4. onmouseout : the onmouseout event is used when a user move the mouse out of an element . 
+
+    function groot5(){
+        document.getElementById("tech2").innerHTML="hello CSS"
+        document.getElementById("tech2").style.backgroundColor="white"
+
+    }
+
+
+
+
+
+
+// 2. Timing events : 
