43a017e3847153f2ef2e5c9df4e9d9897d7c50ac
jatink2000 <jatink8059@gmail.com>
2026-04-24 10:25:44 +0530

    sj

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,32 +1,54 @@
 
 
 
-function Signup(){
+function Signup() {
 
-    let firstnamevalue=document.getElementById("firstname").value
-    let lastnamevalue=document.getElementById("lastname").value
-    let addressvalue=document.getElementById("address").value
-    let cityvalue=document.getElementById("city").value
-    let pincodevalue=document.getElementById("pincode").value
-    let emailvalue=document.getElementById("email").value
-    let passwordvalue=document.getElementById("password").value
+    let firstnamevalue = document.getElementById("firstname").value
+    let lastnamevalue = document.getElementById("lastname").value
+    let addressvalue = document.getElementById("address").value
+    let cityvalue = document.getElementById("city").value
+    let pincodevalue = document.getElementById("pincode").value
+    let emailvalue = document.getElementById("email").value
+    let passwordvalue = document.getElementById("password").value
 
-    let userdata={
-        "firstname":firstnamevalue,
-        "lastname":lastnamevalue, 
-        "address":addressvalue, 
-        "city":cityvalue,
-        "pincode":pincodevalue,
-        "email":emailvalue,
-        "password":passwordvalue
+    let userdata = {
+        "firstname": firstnamevalue,
+        "lastname": lastnamevalue,
+        "address": addressvalue,
+        "city": cityvalue,
+        "pincode": pincodevalue,
+        "email": emailvalue,
+        "password": passwordvalue
     }
 
-    // get the localstorage users : 
-    let alreadyusers=JSON.parse(localStorage.getItem("myusers"))||[]
-    alreadyusers.push(userdata)
+    let alreadyusers = JSON.parse(localStorage.getItem("myusers")) || []
+
+    // email alreday : 
+    let emailalreday = alreadyusers.filter(data => data.email == emailvalue)
 
 
-    // save the data in localStorage :
-        localStorage.setItem("myusers",JSON.stringify(alreadyusers))
+
+    if (!firstnamevalue || !lastnamevalue || !addressvalue || !cityvalue || !pincodevalue || !emailvalue || !passwordvalue) {
+        Swal.fire({
+            icon: "error",
+            title: "Fill The Form!",
+        });
+    }
+    else if (emailalreday.length > 0) {
+        Swal.fire({
+            icon: "error",
+            title: "already user..",
+        });
+    }
+    else {
+        alreadyusers.push(userdata)
+        localStorage.setItem("myusers", JSON.stringify(alreadyusers))
+
+        Swal.fire({
+            title: "Signup Success..",
+            icon: "success"
+        });
+    }
+
 
 }
